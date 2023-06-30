ed6b9e0dbe020397f8edc6f4680a653780f16f8c
Filip Gwardecki <f.gwardecki@quickersim.com>
2023-06-30 14:12:30 +0200

    optimize optimization

--- include/geneticOptimization.hpp
+++ include/geneticOptimization.hpp
@@ -123,5 +123,97 @@ Chromosome geneticOptimizationTBB(double (*evaluate)(double)) {
     return population[0];
 }
 
+template <size_t PopulationSize, size_t NumGenerations, double MutationRate>
+Chromosome geneticOptimizationSerialOptimized(double (*evaluate)(double)) {
+    // Create a random number generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist(-10.0, 10.0);
 
+    // Create an initial population of chromosomes
+    std::array<Chromosome, PopulationSize> population;
+    for (Chromosome& chromosome : population) {
+        chromosome.x = dist(gen);
+    }
+
+    // Main genetic algorithm loop
+    for (size_t generation = 0; generation < NumGenerations; ++generation) {
+        // Evaluate the fitness of each chromosome in the population
+        for (Chromosome& chromosome : population) {
+            chromosome.fitness = evaluate(chromosome.x);
+        }
+
+        // Sort the population based on fitness (ascending order)
+        constexpr size_t numParents = static_cast<size_t>(PopulationSize * 0.2);
+        std::partial_sort(population.begin(), population.begin() + numParents, population.end(),
+          [](const Chromosome& a, const Chromosome& b) {
+          return a.fitness < b.fitness;
+        });
+
+        // Create the next generation through crossover and mutation
+        size_t nextIndex = numParents;
+        while (nextIndex < PopulationSize) {
+            const Chromosome& parent1 = population[std::uniform_int_distribution<size_t>(0, numParents - 1)(gen)];
+            const Chromosome& parent2 = population[std::uniform_int_distribution<size_t>(0, numParents - 1)(gen)];
+            Chromosome child;
+            child.x = (parent1.x + parent2.x) / 2.0;  // Crossover
+            if (std::uniform_real_distribution<double>(0.0, 1.0)(gen) < MutationRate) {
+                child.x += std::uniform_real_distribution<double>(-1.0, 1.0)(gen);  // Mutation
+            }
+            population[nextIndex++] = child;
+        }
+
+    }
+
+    return population[0];
+}
+
+template <size_t PopulationSize, size_t NumGenerations, double MutationRate>
+Chromosome geneticOptimizationTBBOptimized(double (*evaluate)(double)) {
+    tbb::enumerable_thread_specific<std::mt19937> gen([]() {
+        std::random_device rd;
+        return std::mt19937(rd());
+    });
+    std::uniform_real_distribution<double> dist(-10.0, 10.0);
+
+    // Create an initial population of chromosomes
+     std::array<Chromosome, PopulationSize> population;
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, PopulationSize), [&](const tbb::blocked_range<size_t>& range) {
+        auto& thread_gen = gen.local();
+        for (size_t i = range.begin(); i < range.end(); ++i) {
+            population[i].x = dist(thread_gen);
+        }
+    });
+
+    // Main genetic algorithm loop
+    for (size_t generation = 0; generation < NumGenerations; ++generation) {
+        // Evaluate the fitness of each chromosome in the population in parallel
+        tbb::parallel_for(tbb::blocked_range<size_t>(0, PopulationSize), [&](const tbb::blocked_range<size_t>& range) {
+          for (size_t i = range.begin(); i < range.end(); ++i) {
+              population[i].fitness = evaluate(population[i].x);
+          }
+        });
+
+        // Sort the population based on fitness (ascending order)
+        constexpr size_t numParents = static_cast<size_t>(PopulationSize * 0.2);
+        std::partial_sort(population.begin(), population.begin() + numParents, population.end(),
+            [](const Chromosome& a, const Chromosome& b) {
+                return a.fitness < b.fitness;
+            });
+        // Create the next generation through crossover and mutation
+        tbb::parallel_for(numParents, PopulationSize, [&](size_t i) {
+            auto& thread_gen = gen.local();
+            const Chromosome& parent1 = population[std::uniform_int_distribution<size_t>(0, numParents - 1)(thread_gen)];
+            const Chromosome& parent2 = population[std::uniform_int_distribution<size_t>(0, numParents - 1)(thread_gen)];
+            Chromosome child;
+            child.x = (parent1.x + parent2.x) / 2.0;  // Crossover
+            if (std::uniform_real_distribution<double>(0.0, 1.0)(thread_gen) < MutationRate) {
+                child.x += std::uniform_real_distribution<double>(-1.0, 1.0)(thread_gen);  // Mutation
+            }
+            population[i] = child;
+        });
+    }
+
+    return population[0];
+}
 #endif // GENETIC_OPTIMIZATION_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,29 @@ static void BM_TBB(benchmark::State& state) {
     }
 }
 
+template <size_t populationSize, size_t numGenerations, double mutationRate>
+static void BM_serial_Optimized(benchmark::State& state) {
+    // Run the benchmark for each state iteration
+    for (auto _ : state) {
+        // Perform genetic optimization with the specified parameters and fitness function
+        auto bestChromosome = geneticOptimizationSerialOptimized<populationSize, numGenerations, mutationRate>(rastriginFunction);
+
+        // Do not optimize away the result, as it may affect the benchmark
+        benchmark::DoNotOptimize(bestChromosome);
+    }
+}
+
+template <size_t populationSize, size_t numGenerations, double mutationRate>
+static void BM_TBB_Optimized(benchmark::State& state) {
+    // Run the benchmark for each state iteration
+    for (auto _ : state) {
+        // Perform genetic optimization with the specified parameters and fitness function
+        auto bestChromosome = geneticOptimizationTBBOptimized<populationSize, numGenerations, mutationRate>(rastriginFunction);
+
+        // Do not optimize away the result, as it may affect the benchmark
+        benchmark::DoNotOptimize(bestChromosome);
+    }
+}
 
 // BENCHMARK(BM_serial<100, 100, 0.1>)->Unit(benchmark::kMillisecond);
 // BENCHMARK(BM_serial<1000, 100, 0.1>)->Unit(benchmark::kMillisecond);
@@ -51,5 +74,7 @@ BENCHMARK(BM_serial<10000, 10000, 0.4>)->Unit(benchmark::kMillisecond);
 // BENCHMARK(BM_TBB<100, 100, 0.4>)->Unit(benchmark::kMillisecond);
 
 BENCHMARK(BM_TBB<10000, 10000, 0.4>)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_serial_Optimized<10000, 10000, 0.4>)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_TBB_Optimized<10000, 10000, 0.4>)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
